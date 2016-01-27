654aa29b869a882962d2a1b7b7105cdfa4a80136
jbiehler <jbiehler@samhammer.de>
2016-01-27 19:47:06 +0100

    shift items and add some nice mouse over effect

--- app/js/customBindings.js
+++ app/js/customBindings.js
@@ -24,4 +24,52 @@ define(function() {
 			}
 		}
 	};
+
+	ko.bindingHandlers.shift = {
+		init: function (element, valueAccessor) {
+			var value = ko.unwrap(valueAccessor());
+			var settings = ko.utils.extend({
+				list: ko.observableArray(),
+				time: 0
+			}, value);
+			var $e = $(element);
+
+			var $scrollable = $e.find(".scrollable");
+			var realHeight = $e[0].scrollHeight;
+			var trimmed = $e[0].height;
+
+			if(settings.list().length > 0) {
+				var interval = setInterval(function() {
+					if($e.find(":hover").length === 0) {
+						var underlying = settings.list();
+						var toShift = underlying.splice(0, 1);
+						underlying.push(toShift[0]);
+						settings.list.valueHasMutated();
+					}
+				}, settings.time);
+				$(element).data("ko_shift_interval", interval);
+			} else {
+
+			}
+
+			ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+				var interval = $e.data("ko_shift_interval");
+				if (interval) {
+					clearInterval(interval);
+				}
+				$e.removeData("ko_shift_interval");
+			});
+		},
+		update: function(element, valueAccessor) {
+			var $e = $(element);
+			var interval = $e.data("ko_shift_interval");
+
+			if (interval) {
+				clearInterval(interval);
+				$e.removeData("ko_shift_interval");
+			}
+
+			ko.bindingHandlers.shift.init(element, valueAccessor);
+		}
+	};
 });
